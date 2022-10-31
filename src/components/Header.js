affc977345f03567a180f6e62c7a92ed18992d2d
JackCaio <caio.iack1@gmail.com>
2022-10-31 17:32:41 -0300

    feat: Adiciona Header à wallet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { user, expenses } = this.props;
     return (
-      <div>Header</div>
+      // <div>Header</div>
+      <div>
+        <h6 data-testid="email-field">{user}</h6>
+        <p>
+          <span data-testid="total-field">{expenses.length === 0 && 0}</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+      </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.number).isRequired,
+  user: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Header);
