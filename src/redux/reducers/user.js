f8e27770d314f218c92ab171c653b6473bb0ad13
JackCaio <caio.iack1@gmail.com>
2022-10-31 15:20:24 -0300

    feat: Criado estado global

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,13 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_USER = {
+  email: '',
+};
+
+const user = (state = INITIAL_USER, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default user;
