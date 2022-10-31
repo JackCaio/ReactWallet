c07a2aca798f1d8dfc0434c39d7af917c852ff62
JackCaio <caio.iack1@gmail.com>
2022-10-31 16:44:23 -0300

    feat: Preparadas rotas do sistema

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  // return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ Wallet } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
+  );
 }
 
 export default App;
