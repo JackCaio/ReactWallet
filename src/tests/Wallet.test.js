57c40d072015163007233b9e0b93fc0d87ef7610
JackCaio <caio.iack1@gmail.com>
2022-10-31 21:06:46 -0300

    feat: Adiciona testes de formulario da wallet

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,16 +1,35 @@
 import { screen } from '@testing-library/react';
 import Wallet from '../pages/Wallet';
+import mockData from './helpers/mockData';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
+const mail = 'test@test.com';
+const currencies = Object.keys(mockData).filter((currency) => currency !== 'USDT');
 describe('Verifica componentes da wallet', () => {
   test('Verifica estado inicial de Header', () => {
-    const INITIAL_STATE = { user: { email: 'teste@teste.com' } };
+    const INITIAL_STATE = { user: { email: mail } };
     renderWithRouterAndRedux(<Wallet />, { initialState: INITIAL_STATE });
     const user = screen.getByTestId('email-field');
-    expect(user).toHaveTextContent('teste@teste.com');
+    expect(user).toHaveTextContent(mail);
     const total = screen.getByTestId('total-field');
     expect(total).toHaveTextContent('0');
     const headerCurrency = screen.getByTestId('header-currency-field');
     expect(headerCurrency).toHaveTextContent('BRL');
+  });
+
+  test('Testa formulário de adição de despesa', () => {
+    const INITIAL_STATE = { user: { email: mail },
+      wallet: { expenses: [], currencies } };
+    renderWithRouterAndRedux(<Wallet />, { initialState: INITIAL_STATE });
+    const value = screen.getByTestId('value-input');
+    const desc = screen.getByTestId('description-input');
+    const currency = screen.getByTestId('currency-input');
+    const method = screen.getByTestId('method-input');
+    const tag = screen.getByTestId('tag-input');
+    expect(value).toHaveValue(0);
+    expect(desc).toHaveValue('');
+    expect(currency).toHaveValue('USD');
+    expect(method).toHaveValue('dinheiro');
+    expect(tag).toHaveValue('alimentacao');
   });
 });
